6fbbecb1be476933517d0c6cfeb19f8f1a294cac
Foxyes <108577021+lilFoxyes@users.noreply.github.com>
2023-09-10 13:14:31 -0300

    Add files via upload

---
+++ exercise7.py
@@ -0,0 +1,4 @@
+input("Escreva as suas notas e te darei a média do bimestre")
+n1 = int(input("Escreva a sua primeira nota:"))
+n2 = int(input("Escreva a sua segunda nota:"))
+print("A sua média bimestral é: {}".format((n1+n2)/2))
